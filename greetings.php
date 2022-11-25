34c1b0aec548a706a7f1bc889f9e75f2cbc72482
MaTtiaa01 <mattia2001@live.it>
2022-11-25 15:19:28 +0100

    print newstring and newstring length

--- greetings.php
+++ greetings.php
@@ -4,6 +4,9 @@ $paragrafo = $_GET['paragrafo'];
 $parola = $_GET['parola'];
 //var_dump($paragrafo);
 
-$lenght = strlen($paragrafo);
+$parLength = strlen($paragrafo);
+$newString = str_ireplace($parola, '***', $paragrafo);
+$newParLen = strlen($newString);
 
-echo "Il paragrafo: $paragrafo è lungo $lenght caratteri";
+echo "Il paragrafo: $paragrafo è lungo $parLength caratteri"."<br>";
+echo "Il nuovo paragrafo: $newString è lungo $newParLen caratteri";
